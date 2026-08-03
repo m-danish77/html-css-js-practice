89268dd44163e8c2328381257c35e56b582ca31b
m-danish77 <mmunibdanish456@gmail.com>
2026-08-03 12:00:00 +0500

    9th Question solve of JS Practice

---
+++ Javascript/9.js
@@ -0,0 +1,11 @@
+// Solving 9th Question
+
+function reverseString(str) {
+  let reverse = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+  console.log(reverse);
+}
+
+reverseString("hello");
