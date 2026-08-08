67c065e7d19ccaefee81877e64c2b5e0ce092052
m-danish77 <mmunibdanish456@gmail.com>
2026-08-08 12:00:00 +0500

    14th Question solve of JS Practice

---
+++ Javascript/14.js
@@ -0,0 +1,7 @@
+// Solving 14th Question
+
+function containsValue(arr, value) {
+  return arr.includes(value);
+}
+console.log(containsValue([10, 20, 30], 100));
+console.log(containsValue([10, 20, 30], 20));
