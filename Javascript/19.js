aaa1c5fe9089afd9140b79701e929b3dfd042773
m-danish77 <mmunibdanish456@gmail.com>
2026-08-13 12:00:00 +0500

    19th Question solve of JS Practice

---
+++ Javascript/19.js
@@ -0,0 +1,6 @@
+// Solving 19th Exercise
+
+const numbers = [10, 20, 30, 40, 50];
+console.log(numbers.some((num) => num > 40));
+console.log(numbers.every((num) => num > 5));
+console.log(numbers.every((num) => num % 2 == 0));
