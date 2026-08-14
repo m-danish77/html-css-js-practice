b74c44ffaad90edbb997a4f50f96f2b7ea44d69c
m-danish77 <mmunibdanish456@gmail.com>
2026-08-14 12:00:00 +0500

    21 Question solve of JS Practice

---
+++ Javascript/21.js
@@ -0,0 +1,13 @@
+// Solving 21 Question
+
+const student = {
+  name: "Danish",
+  age: 23,
+  university: "BZU",
+};
+
+console.log(student.name);
+student.age = 24;
+student.semester = 7;
+delete student.university;
+console.log(student);
