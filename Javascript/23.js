de581b81a9dabc55640bf6fbd9a86a83dd84e6c5
m-danish77 <mmunibdanish456@gmail.com>
2026-08-16 12:00:00 +0500

    23 Question solve of JS Practice

---
+++ Javascript/23.js
@@ -0,0 +1,16 @@
+// Solving 23 Question
+
+const user = {
+  name: "Danish",
+  address: {
+    city: "Lahore",
+    country: "Pakistan",
+  },
+};
+
+console.log(user.name);
+console.log(user.address.city);
+console.log(user.address.country);
+
+user.address.city = "Multan";
+console.log(user);
